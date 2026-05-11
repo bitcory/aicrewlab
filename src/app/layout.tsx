b1e652057e3bf6fcbc327c1bdfdcebfa37f587d8
bitcory <bitcory0224@gmail.com>
2026-05-12 07:30:23 +0900

    폰트변경
    11

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,60 @@
 import type { Metadata } from "next";
-import { Geist_Mono, Noto_Sans_KR } from "next/font/google";
+import localFont from "next/font/local";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
 import "./globals.css";
 
-const pretendard = Noto_Sans_KR({
-  variable: "--font-pretendard",
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "900"],
+const paperlogy = localFont({
+  variable: "--font-paperlogy",
   display: "swap",
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-  display: "swap",
+  src: [
+    {
+      path: "../../public/fonts/Paperlogy-1Thin.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Paperlogy-2ExtraLight.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Paperlogy-3Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Paperlogy-4Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Paperlogy-5Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Paperlogy-6SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Paperlogy-7Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Paperlogy-8ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Paperlogy-9Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
 });
 
 export const metadata: Metadata = {
@@ -43,7 +82,7 @@ export default function RootLayout({
     <html
       lang="ko"
       suppressHydrationWarning
-      className={`${pretendard.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${paperlogy.variable} h-full antialiased`}
     >
       <body
         suppressHydrationWarning
